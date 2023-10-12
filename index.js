86475d098c817b69525b309728f357c8216b73d5
Niko <135546206+GoodGuyNiko@users.noreply.github.com>
2023-10-12 19:20:32 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ const app = express ();
 const port = 3000;
 const API_URL = "https://api.kanye.rest"
 
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+    try {
+      const response = await axios.get("https://api.kanye.rest");
+      const result = response.data;
+      res.render("index.ejs", { data: result });
+    } catch (error) {
+      console.error("Failed to make request:", error.message);
+      res.render("index.ejs", {
+        error: error.message,
+      });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
+  
